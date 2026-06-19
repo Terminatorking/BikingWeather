83741aaa78b60d1fabc6fff2c578f5f63c504f2a
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-06-19 21:43:48 +0330

    delete unnecessary theme

--- app/src/main/java/ghazimoradi/soheil/bikingweather/presentation/ui/theme/Color.kt
+++ app/src/main/java/ghazimoradi/soheil/bikingweather/presentation/ui/theme/Color.kt
@@ -3,14 +3,6 @@ package ghazimoradi.soheil.bikingweather.presentation.ui.theme
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val Mirage = Color(0xFF0f172a)
 val Cosmos = Color(0xFF1e293b)
 val TranquilBlack = Color(0xFF334155)
